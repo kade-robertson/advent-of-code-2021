98071393d162fdea73f369e52a7ccd2f7ae93ec0
Kade Robertson <kade@kaderobertson.dev>
2021-12-01 17:01:50 -0500

    solve day 1

--- src/problem_01.rs
+++ src/problem_01.rs
@@ -9,8 +9,26 @@ impl Problem01 {
         Problem01 {}
     }
 
-    fn solve_actual(&self, _measurements: &Vec<i64>) -> i64 {
-        return 0;
+    fn solve_actual(&self, measurements: &Vec<i64>) -> i64 {
+        let mut increases = 0;
+        let mut window = measurements.windows(2);
+        while let Some([prev, next]) = window.next() {
+            if next > prev {
+                increases += 1;
+            }
+        }
+        return increases;
+    }
+
+    fn solve_actual_part2(&self, measurements: &Vec<i64>) -> i64 {
+        let mut increases = 0;
+        let mut window = measurements.windows(4);
+        while let Some([first, second, third, fourth]) = window.next() {
+            if (second + third + fourth) > (first + second + third) {
+                increases += 1;
+            }
+        }
+        return increases;
     }
 }
 
@@ -50,6 +68,56 @@ impl Problem for Problem01 {
         }
 
         let result = self.solve_actual(&measurements);
-        println!("Day 1 Answer: {}", result);
+        let result_part2 = self.solve_actual_part2(&measurements);
+        println!("Day 1 Answer:");
+        println!(" - Part 1: {}", result);
+        println!(" - Part 2: {}", result_part2);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_actual_from_example() {
+        let problem = Problem01::new();
+        let measurements: Vec<i64> = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(problem.solve_actual(&measurements), 7);
+    }
+
+    #[test]
+    fn test_solve_actual_always_increasing() {
+        let problem = Problem01::new();
+        let measurements: Vec<i64> = vec![1, 2, 3, 4, 5, 6, 7];
+        assert_eq!(problem.solve_actual(&measurements), 6);
+    }
+
+    #[test]
+    fn test_solve_actual_always_decreasing() {
+        let problem = Problem01::new();
+        let measurements: Vec<i64> = vec![7, 6, 5, 4, 3, 2, 1];
+        assert_eq!(problem.solve_actual(&measurements), 0);
+    }
+
+    #[test]
+    fn test_solve_actual_part2_from_example() {
+        let problem = Problem01::new();
+        let measurements: Vec<i64> = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(problem.solve_actual_part2(&measurements), 5);
+    }
+
+    #[test]
+    fn test_solve_actual_part2_always_increasing() {
+        let problem = Problem01::new();
+        let measurements: Vec<i64> = vec![1, 2, 3, 4, 5, 6, 7];
+        assert_eq!(problem.solve_actual_part2(&measurements), 4);
+    }
+
+    #[test]
+    fn test_solve_actual_part2_always_decreasing() {
+        let problem = Problem01::new();
+        let measurements: Vec<i64> = vec![7, 6, 5, 4, 3, 2, 1];
+        assert_eq!(problem.solve_actual_part2(&measurements), 0);
     }
 }
