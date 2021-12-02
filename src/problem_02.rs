4b9808225ec33bc992e70d7ebabfefa6f2db82c2
Kade Robertson <kade@kaderobertson.dev>
2021-12-02 09:01:05 -0500

    solve day 2 part 2

--- src/problem_02.rs
+++ src/problem_02.rs
@@ -20,6 +20,22 @@ impl Problem02 {
         });
         return depth * distance;
     }
+
+    fn solve_actual_part2(&self, commands: &Vec<Command>) -> i64 {
+        let mut depth: i64 = 0;
+        let mut distance: i64 = 0;
+        let mut aim: i64 = 0;
+        commands.iter().for_each(|cmd| match cmd.instruction {
+            "forward" => {
+                distance += cmd.amount;
+                depth += aim * cmd.amount;
+            }
+            "down" => aim += cmd.amount,
+            "up" => aim -= cmd.amount,
+            _ => (),
+        });
+        return depth * distance;
+    }
 }
 
 impl Problem for Problem02 {
@@ -71,8 +87,10 @@ impl Problem for Problem02 {
         }
 
         let result = self.solve_actual(&commands);
-        println!("Day 1 Answer:");
+        let result_part2 = self.solve_actual_part2(&commands);
+        println!("Day 2 Answer:");
         println!(" - Part 1: {}", result);
+        println!(" - Part 2: {}", result_part2);
     }
 }
 
@@ -158,5 +176,106 @@ mod tests {
             },
         ];
         assert_eq!(problem.solve_actual(&commands), 0);
+    }
+
+    #[test]
+    fn test_solve_actual_part2_from_example() {
+        let problem = Problem02::new();
+        let commands: Vec<Command> = vec![
+            Command {
+                instruction: "forward",
+                amount: 5,
+            },
+            Command {
+                instruction: "down",
+                amount: 5,
+            },
+            Command {
+                instruction: "forward",
+                amount: 8,
+            },
+            Command {
+                instruction: "up",
+                amount: 3,
+            },
+            Command {
+                instruction: "down",
+                amount: 8,
+            },
+            Command {
+                instruction: "forward",
+                amount: 2,
+            },
+        ];
+        assert_eq!(problem.solve_actual_part2(&commands), 900);
+    }
+
+    #[test]
+    fn test_solve_actual_part2_only_depth() {
+        let problem = Problem02::new();
+        let commands: Vec<Command> = vec![
+            Command {
+                instruction: "down",
+                amount: 5,
+            },
+            Command {
+                instruction: "down",
+                amount: 5,
+            },
+            Command {
+                instruction: "down",
+                amount: 5,
+            },
+            Command {
+                instruction: "up",
+                amount: 5,
+            },
+        ];
+        assert_eq!(problem.solve_actual_part2(&commands), 0);
+    }
+
+    #[test]
+    fn test_solve_actual_part2_only_distance() {
+        let problem = Problem02::new();
+        let commands: Vec<Command> = vec![
+            Command {
+                instruction: "forward",
+                amount: 5,
+            },
+            Command {
+                instruction: "forward",
+                amount: 5,
+            },
+            Command {
+                instruction: "forward",
+                amount: 5,
+            },
+            Command {
+                instruction: "forward",
+                amount: 5,
+            },
+        ];
+        assert_eq!(problem.solve_actual_part2(&commands), 0);
+    }
+
+    #[test]
+    fn test_solve_actual_part2_big_movements() {
+        let problem = Problem02::new();
+        // Picked 2097151 because it's roughly the cube root of max(i64), so
+        // this should be a worst case assuming input doesn't cause overflow.
+        // Cube root because "down" will set aim to 2097151, then "forward"
+        // will set distance to 2097151, and depth to aim * 2097151, then
+        // both are multiplied together for the result.
+        let commands: Vec<Command> = vec![
+            Command {
+                instruction: "down",
+                amount: 2097151,
+            },
+            Command {
+                instruction: "forward",
+                amount: 2097151,
+            },
+        ];
+        assert_eq!(problem.solve_actual_part2(&commands), 9223358842721533951);
     }
 }
