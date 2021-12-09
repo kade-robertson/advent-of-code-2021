42498f29d59b5a1ba611aecf87a739e57d310e89
Kade Robertson <kade@kaderobertson.dev>
2021-12-09 09:02:21 -0500

    solve day 9 part 2

--- src/problem_09.rs
+++ src/problem_09.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, VecDeque};
+
 use crate::problem::Problem;
 
 pub struct Problem09 {}
@@ -14,11 +16,11 @@ impl Problem09 {
             .collect()
     }
 
-    fn solve_actual(&self, heightmap: &Vec<Vec<i64>>) -> i64 {
+    fn basin_centers(&self, heightmap: &Vec<Vec<i64>>) -> Vec<(usize, usize)> {
         let height = heightmap.len();
         let width = heightmap[0].len();
 
-        let mut total = 0;
+        let mut basins = Vec::new();
         for i in 0..height {
             for j in 0..width {
                 if i > 0 && heightmap[i - 1][j] <= heightmap[i][j] {
@@ -33,10 +35,61 @@ impl Problem09 {
                 if j < width - 1 && heightmap[i][j + 1] <= heightmap[i][j] {
                     continue;
                 }
-                total += heightmap[i][j] + 1;
+                basins.push((i, j));
             }
         }
-        return total;
+        return basins;
+    }
+
+    fn solve_actual(&self, heightmap: &Vec<Vec<i64>>) -> i64 {
+        let basin_centers = self.basin_centers(heightmap);
+
+        return basin_centers
+            .iter()
+            .fold(0, |acc, (x, y)| acc + heightmap[*x][*y] + 1);
+    }
+
+    fn basin_bfs(&self, heightmap: &Vec<Vec<i64>>, basin_center: (usize, usize)) -> i64 {
+        let height = heightmap.len();
+        let width = heightmap[0].len();
+        let mut seen_pos: HashSet<(usize, usize)> = HashSet::new();
+        let mut to_be_checked: VecDeque<(usize, usize)> = VecDeque::new();
+        to_be_checked.push_back(basin_center);
+
+        while to_be_checked.len() > 0 {
+            let (x, y) = to_be_checked.pop_front().unwrap();
+            if seen_pos.contains(&(x, y)) {
+                continue;
+            }
+            seen_pos.insert((x, y));
+
+            if x > 0 && heightmap[x - 1][y] < 9 {
+                to_be_checked.push_back((x - 1, y));
+            }
+            if x < height - 1 && heightmap[x + 1][y] < 9 {
+                to_be_checked.push_back((x + 1, y));
+            }
+            if y > 0 && heightmap[x][y - 1] < 9 {
+                to_be_checked.push_back((x, y - 1));
+            }
+            if y < width - 1 && heightmap[x][y + 1] < 9 {
+                to_be_checked.push_back((x, y + 1));
+            }
+        }
+
+        return seen_pos.len() as i64;
+    }
+
+    fn solve_actual_part2(&self, heightmap: &Vec<Vec<i64>>) -> i64 {
+        let basin_centers = self.basin_centers(heightmap);
+        let mut basin_sizes: Vec<i64> = basin_centers
+            .iter()
+            .map(|pos| self.basin_bfs(heightmap, *pos))
+            .collect();
+
+        basin_sizes.sort();
+        let length = basin_sizes.len();
+        return basin_sizes[length - 1] * basin_sizes[length - 2] * basin_sizes[length - 3];
     }
 }
 
@@ -52,7 +105,9 @@ impl Problem for Problem09 {
     }
 
     fn solve_part2(&self) -> i64 {
-        0
+        let input = get_input!("./inputs/problem_09.txt");
+        let heightmap = self.parse(input);
+        return self.solve_actual_part2(&heightmap);
     }
 }
 
@@ -66,5 +121,29 @@ mod tests {
         let input = get_input!("./inputs/problem_09_example.txt");
         let heightmap = problem.parse(input);
         assert_eq!(problem.solve_actual(&heightmap), 15);
+    }
+
+    #[test]
+    fn test_solve_actual_from_input() {
+        let problem = Problem09::new();
+        let input = get_input!("./inputs/problem_09.txt");
+        let heightmap = problem.parse(input);
+        assert_eq!(problem.solve_actual(&heightmap), 562);
+    }
+
+    #[test]
+    fn test_solve_actual_part2_from_example() {
+        let problem = Problem09::new();
+        let input = get_input!("./inputs/problem_09_example.txt");
+        let heightmap = problem.parse(input);
+        assert_eq!(problem.solve_actual_part2(&heightmap), 1134);
+    }
+
+    #[test]
+    fn test_solve_actual_part2_from_input() {
+        let problem = Problem09::new();
+        let input = get_input!("./inputs/problem_09.txt");
+        let heightmap = problem.parse(input);
+        assert_eq!(problem.solve_actual_part2(&heightmap), 1076922);
     }
 }
